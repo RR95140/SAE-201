8d4131e250ac385ccee71871503c0db8a0c52842
RAMASSAMY Ryan <rayourama@hotmail.com>
2023-06-14 21:40:48 +0200

    Modification de la classe trianglesTest

--- src/test/trianglesTest.java
+++ src/test/trianglesTest.java
@@ -7,31 +7,37 @@ import java.util.ArrayList;
 
 
 public class trianglesTest {
-    @Test
-    public void testDefaultConstructor() {
+    
+	// Définition des test des constructeurs
+	
+	// Test du constructeur par défaut
+	@Test
+    public void trianglesTest() {
         triangles triangle = new triangles();
-        Assert.assertNull(triangle.getTrianglePoint1());
-        Assert.assertNull(triangle.getTrianglePoint2());
-        Assert.assertNull(triangle.getTrianglePoint3());
+        Assert.assertNull(triangle.getPoint1());
+        Assert.assertNull(triangle.getPoint2());
+        Assert.assertNull(triangle.getPoint3());
         Assert.assertNull(triangle.getNom());
     }
 
-    @Test
-    public void testParameterizedConstructor() {
+	// Test du constructeur avec paramètres
+	@Test
+    public void trianglesTest() {
         PointPlan p1 = new PointPlan(0, 0);
         PointPlan p2 = new PointPlan(5, 0);
         PointPlan p3 = new PointPlan(0, 5);
 
         triangles triangle = new triangles("MonTriangle", p1, p2, p3);
 
-        Assert.assertEquals(p1, triangle.getTrianglePoint1());
-        Assert.assertEquals(p2, triangle.getTrianglePoint2());
-        Assert.assertEquals(p3, triangle.getTrianglePoint3());
+        Assert.assertEquals(p1, triangle.getPoint1());
+        Assert.assertEquals(p2, triangle.getPoint2());
+        Assert.assertEquals(p3, triangle.getPoint3());
         Assert.assertEquals("MonTriangle", triangle.getNom());
     }
-
+	
+	// Test du constructeur par copie
     @Test
-    public void testCopyConstructor() {
+    public void trianglesTest() {
         PointPlan p1 = new PointPlan(0, 0);
         PointPlan p2 = new PointPlan(5, 0);
         PointPlan p3 = new PointPlan(0, 5);
@@ -39,11 +45,15 @@ public class trianglesTest {
         triangles originalTriangle = new triangles("MonTriangle", p1, p2, p3);
         triangles copiedTriangle = new triangles(originalTriangle);
 
-        Assert.assertNotEquals(originalTriangle.getTrianglePoint1(), copiedTriangle.getTrianglePoint1());
-        Assert.assertNotEquals(originalTriangle.getTrianglePoint2(), copiedTriangle.getTrianglePoint2());
-        Assert.assertNotEquals(originalTriangle.getTrianglePoint3(), copiedTriangle.getTrianglePoint3());
+        Assert.assertNotEquals(originalTriangle.getPoint1(), copiedTriangle.getPoint1());
+        Assert.assertNotEquals(originalTriangle.getPoint2(), copiedTriangle.getPoint2());
+        Assert.assertNotEquals(originalTriangle.getPoint3(), copiedTriangle.getPoint3());
         Assert.assertEquals(originalTriangle.getNom(), copiedTriangle.getNom());
     }
+    
+	// Définition des tests de fonctions
+    
+    // Test de la fonction dessiner
     @Test
     public void testDessiner() {
         PointPlan p1 = new PointPlan(0, 0);
@@ -58,6 +68,7 @@ public class trianglesTest {
 
     }
 
+    // Test de la fonction déplacer
     @Test
     public void testDeplacer() {
         PointPlan p1 = new PointPlan(0, 0);
@@ -68,13 +79,14 @@ public class trianglesTest {
 
         triangle.deplacer(10, 10);
 
-        PointPlan newP1 = new PointPlan(10, 10);
-        PointPlan newP2 = new PointPlan(15, 10);
-        PointPlan newP3 = new PointPlan(10, 15);
+        PointPlan p4 = new PointPlan(10, 10);
+        PointPlan p5 = new PointPlan(15, 10);
+        PointPlan p6 = new PointPlan(10, 15);
 
 
     }
 
+    // Test de la fonction pour récupérer le nom de la forme
     @Test
     public void testGetNomForme() {
         triangles triangle = new triangles();
